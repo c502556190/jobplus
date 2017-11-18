c52a228229893669d43060232e9b08baadf19668
xujiaqi <xujiaqi@xujiaqideMacBook-Pro.local>
2017-11-18 10:01:32 +0800

    增加企业和职位的功能模块

--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -2,6 +2,45 @@ from flask import Blueprint
 
 company = Blueprint('company', __name__, url_prefix='/company')
 
+
 @company.route('/')
 def index():
     return 'Company'
+
+
+@company.route('/list/', methods=["GET", "POST"])
+def CompanyListView():
+    """
+    企业列表页
+    :return:
+    """
+    return 'CompanyList'
+
+
+@company.route('/detail/<int:id>', methods=["GET", "POST"])
+def CompanyDetailView(id):
+    """
+    企业详情页
+    :param id: 企业id
+    :return:
+    """
+    return 'CompanyDetailView{}'.format(id)
+
+
+@company.route('/Position/', methods=["GET", "POST"])
+def CompanyPosition():
+    """
+    职位管理页
+    :return:
+    """
+    return 'Position'
+
+
+@company.route('/cv/', methods=["GET", "POST"])
+def CompanyCv():
+    """
+    简历管理页
+    :return:
+    """
+    return 'Companycv'
+
--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -2,6 +2,27 @@ from flask import Blueprint
 
 job = Blueprint('job', __name__, url_prefix='/job')
 
+
 @job.route('/')
 def index():
     return 'Job'
+
+
+@job.route('/list/', methods=["GET", "POST"])
+def JobListView():
+    """
+    职位列表
+    :return:
+    """
+    return 'JobList'
+
+
+@job.route('/detail/<int:id>', methods=["GET", "POST"])
+def JobDetailView(id):
+    """
+    职位详情
+    :param id: 职位id
+    :return:
+    """
+    return 'JobDetail:{}'.format(id)
+
