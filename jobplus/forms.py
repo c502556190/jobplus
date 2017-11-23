15d769329e8c8e69035a6d44e599b7c75e03938a
1413400908 <1413400908@qq.com>
2017-11-23 15:14:18 +0800

    create login/register/  form and page

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, PasswordField, SubmitField, SelectField, FileField)
 from wtforms.validators import (Length, Email, DataRequired)
+from wtforms import (StringField, PasswordField, SubmitField, SelectField, Field,BooleanField,ValidationError,TextAreaField,IntegerField)
+from wtforms.validators import (Length, Email, DataRequired, Required,URL,NumberRange,EqualTo)
 from jobplus.models import db, User
 
 
@@ -66,3 +68,44 @@ class UserProfileForm(FlaskForm):
         label='提交'
     )
 
+
+
+    # 创建用户登录和注册表单 Author:小学生
+
+class LoginForm(FlaskForm):
+    email = StringField('邮箱',validators=[Required(),Email()])
+    password = PasswordField('密码',validators=[Required(),Length(6,24)])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('提交')
+    def validate_email(self,field):
+        if field.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError('该邮箱未注册')
+
+    def validate_password(self,field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError('密码错误')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('用户名',validators=[Required(),Length(3,24)])
+    email = StringField('邮箱',validators=[Required(),Email()])
+    password = PasswordField('密码',validators=[Required(),Length(6,24)])
+    repeat_password=PasswordField('重复密码',validators=[Required(),EqualTo('password')])
+    submit = SubmitField('提交')
+
+    def validate_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名以及存在')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
+
+    def create_user(self):
+        user =User(username=self.username.data,
+                   email=self.email.data,
+                   password=self.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return user
--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template
 from jobplus.forms import UserProfileForm
+#引用 author:小学生
+from jobplus.models import User
+from flask_login import login_user,logout_user,login_required
+from flask import redirect,url_for,request,current_app
+from jobplus.forms import LoginForm,RegisterForm
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
@@ -15,4 +20,31 @@ def profile():
     if form.validate_on_submit():
         pass
     return render_template('user/profile.html', form=form)
+
+# 添加用户注册和登录和登出路由 author:小学生
+@user.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        login_user(user,form.remember_me.data)
+        return redirect(url_for('.profile'))
+    return render_template('login.html',form=form)
+
+@user.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('register success，login please!','success')
+        return redirect(url_for('.login'))
+    return render_template('register.html',form=form)
+
+@user.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('quited','success')
+    return redirect(url_for('.index'))
+    
 
