c3ca2a1abb16e47a084ea7d49969c5eedcb99e5f
xujiaqi <xujiaqi@xujiaqideMacBook-Pro.local>
2017-11-23 20:14:44 +0800

    update login.html

--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 from jobplus.forms import UserProfileForm
-#引用 author:小学生
+# 引用 author:小学生
 from jobplus.models import User
-from flask_login import login_user,logout_user,login_required
-from flask import redirect,url_for,request,current_app
-from jobplus.forms import LoginForm,RegisterForm
+from flask_login import login_user, logout_user, login_required
+from flask import redirect, url_for, request, current_app
+from jobplus.forms import LoginForm, RegisterForm
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
@@ -21,30 +21,31 @@ def profile():
         pass
     return render_template('user/profile.html', form=form)
 
+
 # 添加用户注册和登录和登出路由 author:小学生
-@user.route('/login',methods=['GET','POST'])
+@user.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user=User.query.filter_by(email=form.email.data).first()
-        login_user(user,form.remember_me.data)
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)
         return redirect(url_for('.profile'))
-    return render_template('login.html',form=form)
+    return render_template('login.html', form=form)
 
-@user.route('/register',methods=['GET','POST'])
+
+@user.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
         form.create_user()
-        flash('register success，login please!','success')
+        flash('register success，login please!', 'success')
         return redirect(url_for('.login'))
-    return render_template('register.html',form=form)
+    return render_template('register.html', form=form)
+
 
 @user.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('quited','success')
+    flash('quited', 'success')
     return redirect(url_for('.index'))
-    
-
