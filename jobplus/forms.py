a194fc63d3780d9fb0e3626798dfbb27581349f6
xujiaqi <xujiaqi@xujiaqideMacBook-Pro.local>
2017-11-23 10:31:03 +0800

    update templates

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,43 +1,76 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,ValidationError,TextAreaField,IntegerField
-from wtforms.validators import Length,Email,EqualTo,Required,URL,NumberRange
-from simpledu.models import db,User,Course
-
-class LoginForm(FlaskForm):
-    email = StringField('邮箱',validators=[Required(),Email()])
-    password = PasswordField('密码',validators=[Required(),Length(6,24)])
-    remember_me = BooleanField('记住我')
-    submit = SubmitField('提交')
-
-    def validate_email(self,field):
-        if field.data and not User.query.filter_by(email=field.data).first():
-            raise ValidationError('该邮箱未注册')
-    def validate_password(self,field):
-        user = User.query.filter_by(email=self.email.data).first()
-        if user and not user.check_password(field.data):
-            raise ValidationError('密码错误')
+from wtforms import (StringField, PasswordField, SubmitField, SelectField, Field)
+from wtforms.validators import (Length, Email, DataRequired, Required)
+from jobplus.models import db, User
 
 
-class RegisterForm(FlaskForm):
-    username =StringField('用户名',validators=[Required(),Length(3,24)])
-    email = StringField('邮箱',validators[Required(),Email()])
-    password =PasswordField('密码',validators[Required(),Length(6,24)])
-    repeat_password = PasswordField('重复密码',validators[Required(),EqualTo('password')])
-    submit = SubmitField('提交')
+class UserProfileForm(FlaskForm):
+    name = StringField(
+        label='用户名',
+        validators=[
+            DataRequired('用户名不能为空!'),
+            Length(3, 24)
+        ],
+        render_kw={
+            "class": "form-control",
+            "id": "exampleInputEmail1",
+            "placeholder": "请输入片名！",
+        }
+    )
 
-    def validate_username(self,field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError('用户已经存在')
+    paswd = PasswordField(
+        label='密码',
+        validators=[
+            DataRequired("密码不能为空!"),
+            Length(6, 24)
+        ],
+        render_kw={
+            'class': 'form-control'
+        }
+    )
 
-    def validate_email(self,field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱已经存在')
+    email = StringField(
+        label='邮箱',
+        validators=[
+            DataRequired('邮箱不能为空!'),
+            Length(1, 64),
+            Email()
+        ],
+        render_kw={
+            'class': 'form-control'
+        }
+    )
 
-    def create_user(self):
-        user = User(username=self.username.data,
-                    email = self.email.data,
-                    password = self.password.data)
-        db.session.add(user)
-        db.sesison.commit()
-        return user
+    job_experiences = SelectField(
+        label="年限",
+        validators=[
+            DataRequired("工作年限!"),
+        ],
+        coerce=int,
+        choices=[
+            (1, "1年"),
+            (2, "2年"),
+            (3, "3年"),
+            (4, "4年"),
+            (5, "5年"),
+
+        ],
+        render_kw={
+            'class': 'form-control'
+        }
+    )
+
+    resume = Field(
+        label='简历',
+        validators=[
+            DataRequired("请上传简历")
+        ],
+        render_kw={
+            'class': 'form-control'
+        }
+    )
+
+    submit = SubmitField(
+        label='提交'
+    )
 
--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from jobplus.forms import UserProfileForm
 
 user = Blueprint('user', __name__, url_prefix='/user')
+
 
 @user.route('/')
 def index():
     return 'User'
+
+
+@user.route('/profile/', methods=["GET", "POST"])
+def profile():
+    form = UserProfileForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('user/profile.html', form=form)
+
