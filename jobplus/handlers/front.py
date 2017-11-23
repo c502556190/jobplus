b9eb90778af75cac7362068996214b8c64e3d2ac
xujiaqi <xujiaqi@xujiaqideMacBook-Pro.local>
2017-11-23 21:08:46 +0800

    update base.html

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
-from flask import render_template
+from flask import (Blueprint, render_template, redirect, url_for)
+from flask_login import (login_user, logout_user, login_required)
+from jobplus.forms import LoginForm
+from jobplus.models import User
 
 front = Blueprint('/', __name__)
 
@@ -8,3 +10,12 @@ front = Blueprint('/', __name__)
 def index():
     return render_template('front/index.html')
 
+
+@front.route('/login/', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)
+        return redirect(url_for('.profile'))
+    return render_template('login.html', form=form)
