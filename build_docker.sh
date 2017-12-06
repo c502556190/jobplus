620fa56c3e957d9bdc6d223509072a544c056524
xujiaqi <xujiaqi@xujiaqideMacBook-Pro.local>
2017-12-06 21:23:35 +0800

    add docker-deploy

---
+++ build_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# != 1 ] ; then
+  echo "USAGE: ./build_docker.sh v1.0.0"
+  exit 1;
+fi
+cp deploy-docker/Dockerfile ../
+cd ../
+docker build -t syl/jobplus:$1 .
+rm Dockerfile
+docker run -d -p 9000:80 --name syl_$1 syl/jobplus:$1
--- manage.py
+++ manage.py
@@ -4,3 +4,8 @@ app = create_app('development')
 
 if __name__ == '__main__':
     app.run()
+    # 使用 gevent 提供的 WSGI 服务器，并启用 WebSocket 支持
+    import pywsgi
+    # 创建一个 WSGIServer，包含我们的 app 和 gevent 的 WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app)
+    server.serve_forever()
